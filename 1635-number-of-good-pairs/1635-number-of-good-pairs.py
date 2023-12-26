49aeef0e460a027730e635c61573e864f8ee3559
Saran Dharshan S.P <sarandharshanpushparaj@gmail.com>
2023-12-26 11:50:45 +0530

    Delete 1635-number-of-good-pairs directory

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def numIdenticalPairs(self, nums: List[int]) -> int:
-        a = len(nums)
-        count = 0
-        for i in range(a):
-            for j in range(i+1,a):
-                if nums[i] == nums[j]:
-                    count+=1
-        return count            
